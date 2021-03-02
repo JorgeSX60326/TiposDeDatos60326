86fc9735af6c45bbd5582d4aa373050a9facb447
Jun <Jun@deivydgeorge>
2021-03-01 20:29:10 -0600

    Commit completo

---
+++ src/tiposdedatos60326/TiposDeDatos60326.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatos60326;
+
+/**
+ *
+ * @author Jun
+ */
+public class TiposDeDatos60326 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+//Imprimiremos los valores minimos y maximos de los siguientes datos: byte, short, int, long, y double.
+        System.out.println(" El valor minimo de byte es de: " + Byte.MIN_VALUE);
+        System.out.println(" El valor maximo de byte es de: " + Byte.MAX_VALUE);
+        System.out.println(" El valor minimo de short es de: " + Short.MIN_VALUE);
+        System.out.println(" El valor maximo de short es de: " + Short.MAX_VALUE);
+        System.out.println(" El valor minimo de int es de: " + Integer.MIN_VALUE);
+        System.out.println(" El valor maximo de int es de: " + Integer.MAX_VALUE);
+        System.out.println(" El valor minimo de long es de: " + Long.MIN_VALUE);
+        System.out.println(" El valor maximo de long es de: " + Long.MAX_VALUE);
+        System.out.println(" El valor minimo de double es de: " + Double.MIN_VALUE);
+        System.out.println(" El valor maximo de double es de: " + Double.MAX_VALUE);
+        
+    }
+    
+}
